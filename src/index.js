ac3efb26a0d29f5b7c7fb72457e2d00ad5c63c4a
Андрей <dronxxx1993@yandex.ru>
2020-10-13 22:45:16 +0400

    feat:solved the reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (arr) {
+    if(arr < 0){
+        arr = arr * -1
+        } else if(arr.length -1 == 0){
+        String(arr).slice(0,-1)
+        console.log(arr)
+        }
+        let arrOne = []
+            let z = String(arr).split('')
+          z.reverse()
+            z.map((item)=>{
+                    arrOne.push(+item)
+            })
+            return +arrOne.join('')
 }
